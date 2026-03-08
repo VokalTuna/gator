9c28192b73809475fd6097e133777a2a255b7dfa
VokalTuna <knutolavs@pm.me>
2026-03-08 13:45:28 +0100

    Able to add feeds to database

--- main.go
+++ main.go
@@ -41,6 +41,7 @@ func main() {
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerUsers)
 	cmds.register("agg", handlerAggregator)
+	cmds.register("addfeed", handleAddfeed)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
